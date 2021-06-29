80d5495b465a7d0a2347ad6f15fa48a9d8af9e9a
yoursamlan <skamlan99@gmail.com>
2021-06-29 20:47:18 +0530

    Neumorphic Tribute Page

--- 1. Responsive Web-design/1. Tribute Page/script.js
+++ 1. Responsive Web-design/1. Tribute Page/script.js
@@ -1,27 +1,38 @@
-// !! IMPORTANT README:
+jQuery(document).ready(function()
+{
+	var navset, scrollPos = 0;
 
-// You may add additional external JS and CSS as needed to complete the project, however the current external resource MUST remain in place for the tests to work. BABEL must also be left in place. 
+	jQuery("nav").wrap('<div class="nav-placeholder"></div>');
+	jQuery("nav").wrapInner('<div class="nav-inner"></div>');
+	jQuery(".nav-inner").wrapInner('<div class="nav-inner-most"></div>');
 
-/***********
-INSTRUCTIONS:
-  - Select the project you would 
-    like to complete from the dropdown 
-    menu.
-  - Click the "RUN TESTS" button to
-    run the tests against the blank 
-    pen.
-  - Click the "TESTS" button to see 
-    the individual test cases. 
-    (should all be failing at first)
-  - Start coding! As you fulfill each
-    test case, you will see them go   
-    from red to green.
-  - As you start to build out your 
-    project, when tests are failing, 
-    you should get helpful errors 
-    along the way!
-    ************/
-
-// PLEASE NOTE: Adding global style rules using the * selector, or by adding rules to body {..} or html {..}, or to all elements within body or html, i.e. h1 {..}, has the potential to pollute the test suite's CSS. Try adding: * { color: red }, for a quick example!
-
-// Once you have read the above messages, you can delete all comments.
+	function stickyUtility()
+	{
+		if (!jQuery("nav").hasClass("fixed"))
+		{
+			navset = jQuery("nav").offset().top;
+		}
+	}
+	
+	stickyUtility();
+	
+	jQuery(window).resize(function()
+	{
+		stickyUtility();
+	});
+	
+	jQuery(window).scroll(function()
+	{
+		scrollPos = jQuery(window).scrollTop();
+		
+		if (scrollPos >= navset)
+		{
+			jQuery("nav").addClass("fixed");
+		}
+		else
+		{
+			jQuery("nav").removeClass("fixed");
+		}
+		
+	});
+});
